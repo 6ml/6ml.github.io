67a8415a02fc96bdaadc6a3aafea8368a3da272f
lupath <849104822@qq.com>
2017-06-13 21:32:16 +0800

    [style]add animated class to page.html

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
       $('.panel-cover').addClass('panel-cover--collapsed');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '320px', 'width': '24%'}, 400, swing = 'swing', function() {} );
     }
 
     
